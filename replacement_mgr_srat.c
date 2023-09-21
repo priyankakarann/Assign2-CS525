e04bd9ad41940c1b9f81ad97cad5d672894a639c
Pratik Jadhav <pratikjadhav@Pratiks-MacBook-Pro.local>
2023-09-20 20:01:01 -0500

    Added replacement strategies files for implementation

---
+++ replacement_mgr_srat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "buffer_mgr.h"
+#include "replacement_mgr_strat.h"
+
+
+void FIFO (BM_BufferPool *const bm){
+
+};
+
+void LRU (BM_PageHandle *const page){
+
+};
+
+void CLOCK (BM_BufferPool *const bm){
+
+};
+
+void LFU (BM_PageHandle *const page){
+
+};
+
+void LRU_k (BM_PageHandle *const page){
+
+};
---
+++ replacement_mgr_strat.h
@@ -0,0 +1,14 @@
+#ifndef REPLACEMENT_MGR_STRAT_H
+#define REPLACEMENT_MGR_STRAT_H
+
+#include "buffer_mgr.h";
+
+// Replacement Strategies
+
+extern void FIFO (BM_BufferPool *const bm);
+extern void LRU (BM_PageHandle *const page);
+extern void CLOCK (BM_BufferPool *const bm);
+extern void LFU (BM_PageHandle *const page);
+extern void LRU_k (BM_PageHandle *const page);
+
+#endif
